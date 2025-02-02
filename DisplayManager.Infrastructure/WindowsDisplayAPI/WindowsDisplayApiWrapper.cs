b6397492c6cb31106a2f4cf83d5c7bcd626d8f3c
Brailly Julien <juju.brailly@orange.fr>
2025-02-02 23:32:57 +0100

    Remove unused using directives in WindowsDisplayApiWrapper.cs to clean up code

--- DisplayManager.Infrastructure/WindowsDisplayAPI/WindowsDisplayApiWrapper.cs
+++ DisplayManager.Infrastructure/WindowsDisplayAPI/WindowsDisplayApiWrapper.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
-using System.Management;
+﻿using System.Runtime.InteropServices;
 using DisplayManager.Domain.Entities;
 
 namespace DisplayManager.Infrastructure.WindowsDisplayAPI;
